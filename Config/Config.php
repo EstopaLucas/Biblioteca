21a70d926391efb988d0da8be14715f8fc268ae1
Lucas Rocha Estopa <estopalucas@outlook.com>
2024-06-21 19:51:18 -0300

    zerando o trabalho
    commit 1

--- Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const base_url = "http://localhost/Biblioteca/";
-const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "biblioteca";
-const charset = "charset=utf8";
-?>
